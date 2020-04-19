f8da8feee7d8bb4870821b6609753ceb59c9b564
Boris Ballester <boris.ballester@adamo.es>
2020-04-19 02:42:35 +0200

    wip

--- src/browser/core/ui/locator.js
+++ src/browser/core/ui/locator.js
@@ -20,14 +20,12 @@ class UILocator
     const
     initialViewModel  = this.getInitialViewModel(),
     treeFactory       = this.locator.locate('data-structure/tree'),
-    hbs               = this.locator.locate('view/handlebars'),
-    channel           = this.locator.locate('infrastructure/bus').createChannel('ui'),
     viewModelToTree   = this.locator.locate('core/mapper/view-model-to-tree'),
     object            = this.locator.locate('core/object'),
     configuration     = this.locator.locate('core/configuration'),
     componentFactory  = this.locator.locate('core/component/factory')
 
-    return new UI(initialViewModel, treeFactory, hbs, channel, viewModelToTree, object, this.locator, configuration, componentFactory)
+    return new UI(initialViewModel, treeFactory, viewModelToTree, object, this.locator, configuration, componentFactory)
   }
 }
 
