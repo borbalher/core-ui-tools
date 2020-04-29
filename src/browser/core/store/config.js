d9d4ece59fb850c41d2591492e024127f44ba9fb
Boris Ballester <boris.ballester@adamo.es>
2020-04-29 22:20:21 +0200

    wip

--- src/browser/core/store/config.js
+++ src/browser/core/store/config.js
@@ -11,7 +11,6 @@ module.exports = {
     locator :
     {
       'core/store'               : __dirname,
-      'core/store/bootstrap'     : `${__dirname}/bootstrap`,
       'store/middleware/logger'  : `${__dirname}/middleware/logger`,
       'store/middleware/delayer' : `${__dirname}/middleware/delayer`
     }
