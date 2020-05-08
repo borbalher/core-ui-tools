56f81c6023618e649ccd7ebd8e288e40fc803ae9
Boris Ballester <boris.ballester@adamo.es>
2020-05-09 01:14:24 +0200

    wip

--- src/browser/core/is-component/index.js
+++ src/browser/core/is-component/index.js
@@ -1,8 +1,7 @@
 module.exports = (element) =>
 {
-  return (typeof element === 'object'           && element)
-         && (element.hasOwnProperty('id')       && element.id)
-         && (element.hasOwnProperty('name')     && element.name)
-         && (element.hasOwnProperty('template') && element.template)
-         && (element.hasOwnProperty('schema')   && element.schema)
+  return (typeof element === 'object'           && element)          &&
+            (element.hasOwnProperty('id')       && element.id)       &&
+            (element.hasOwnProperty('template') && element.template) &&
+            (element.hasOwnProperty('schema')   && element.schema)
 }
