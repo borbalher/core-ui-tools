0c5f11a939140490e87212e6107e8f85980a3e04
Boris Ballester <boris.ballester@adamo.es>
2020-05-18 17:25:46 +0200

    wip

--- src/common/ui/aggregate/component/controller/factory/index.js
+++ src/common/ui/aggregate/component/controller/factory/index.js
@@ -1,6 +1,6 @@
 class ComponentFactory
 {
-  constructor(configuration, bus, locator, store, hbs, deepfind, page)
+  constructor(configuration, bus, locator, store, hbs, deepfind)
   {
     this.configuration = configuration
     this.bus           = bus
@@ -8,7 +8,6 @@ class ComponentFactory
     this.store         = store
     this.hbs           = hbs
     this.deepfind      = deepfind
-    this.page          = page
   }
 
   createBusChannel(id)
@@ -29,7 +28,10 @@ class ComponentFactory
     channel    = this.createBusChannel(id),
     Controller = this.locator.locate(`ui/${template}/controller`)
 
-    return new Controller(id, schema, bindings, listeners, this.bus, this.store, this.hbs,  channel, this.page, this.locator)
+    return (page) =>
+    {
+      return new Controller(id, schema, bindings, listeners, this.bus, this.store, this.hbs,  channel, page, this.locator)
+    }
   }
 }
 
--- src/common/ui/aggregate/component/controller/factory/locator.js
+++ src/common/ui/aggregate/component/controller/factory/locator.js
@@ -13,10 +13,9 @@ class ComponentFactoryLocator
     configuration = this.locator.locate('core/configuration'),
     bus           = this.locator.locate('infrastructure/bus'),
     store         = this.locator.locate('core/store'),
-    page          = this.locator.locate('core/page'),
     hbs           = this.locator.locate('view/handlebars')
 
-    return new ComponentFactory(configuration, bus, this.locator, store, hbs, page)
+    return new ComponentFactory(configuration, bus, this.locator, store, hbs)
   }
 }
 
