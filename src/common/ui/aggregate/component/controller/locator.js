bedcd79412ead07afc771504fe9b7d0fec59a94d
Boris Ballester <boris.ballester@adamo.es>
2020-05-23 13:26:04 +0200

    wip

--- src/common/ui/aggregate/component/controller/locator.js
+++ src/common/ui/aggregate/component/controller/locator.js
@@ -14,7 +14,7 @@ class ComponentFactoryLocator
     bus            = this.locator.locate('infrastructure/bus'),
     store          = this.locator.locate('core/store'),
     hbs            = this.locator.locate('view/handlebars'),
-    actionComposer = this.locator.locate('core/action-composer')
+    actionComposer = this.locator.locate('core/action/composer')
 
     return new ComponentFactory(configuration, bus, this.locator, store, hbs, actionComposer)
   }
